b53961900d1af601dbb446306cc836419976ef67
Rodrigo Pestana <58884259+rpestana18@users.noreply.github.com>
2022-05-22 14:56:32 -0300

    Bug Removed

--- app.py
+++ app.py
@@ -7,16 +7,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Up the Irons! \m/\m/"
-
-@app.route('/bug')                                                                                                                                
-def bad():                                                                                                                                        
-    try:                                                                                                                                          
-        raise TypeError()                                                                                                                         
-    except TypeError as e:                                                                                                                        
-        print(e)                                                                                                                                  
-    except TypeError as e:                                                                                                                        
-        print("Duplicado, ou seja, nunca vai entrar aqui.")    
+    return "Up the Irons! \m/\m/"   
 
 if __name__ == '__main__':
     app.run()
