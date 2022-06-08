7717d49d1072e9f5aad882f54a647cd84ec37fe9
Rodrigo Pestana <58884259+rpestana18@users.noreply.github.com>
2022-06-08 17:52:21 -0300

    branch docker

--- app.py
+++ app.py
@@ -10,4 +10,5 @@ def pagina_inicial():
     return "Grupo 22 4Winds -Danilo Caporal -Gicele Castro -Jeremias Oliveira -Rodrigo Pestana"   
 
 if __name__ == '__main__':
-    app.run()
+    port = os.getenv('PORT')
+    app.run('0.0.0.0', port=port)
